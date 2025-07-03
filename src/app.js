44ae02703872530d2bbbbc55bca9f845b84f2719
Oliver <softwaredevdad@gmail.com>
2025-07-03 21:57:08 +0200

    Test Commit

--- src/app.js
+++ src/app.js
@@ -15,21 +15,21 @@ app.use(
   cors({
     origin: "process.env.CORS_ORIGIN",
     credentials: true,
-  })
+  }),
 );
 
 //NOTE: Allows json data to pass through, but with a limit, so it's not unlimited data
 app.use(
   express.json({
     limit: "16kb",
-  })
+  }),
 );
 //NOTE:
 app.use(
   express.urlencoded({
     extended: true,
     limit: "16kb",
-  })
+  }),
 );
 //NOTE: Serving our static files so they can be rendered on the webpage
 app.use(express.static("public"));
