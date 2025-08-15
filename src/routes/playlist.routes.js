1dd96c297788daae6b66710ee23fc4a75de33810
Oliver <softwaredevdad@gmail.com>
2025-08-15 22:44:55 +0200

    clean up routes

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -12,12 +12,23 @@ import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
+//#region Get Routes
 router.get("/playlists", verifyJWT, getUserPlaylists);
+router.get("/p/:id", verifyJWT, getPlaylistById);
+//#endregion
+
+//#region Post Routes
 router.post("/create", verifyJWT, createPlaylist);
 router.post("/playlist", verifyJWT, addVideoToPlaylist);
-router.get("/p/:id", verifyJWT, getPlaylistById);
-router.delete("/delete/:playlistId", verifyJWT, deletePlaylist);
+//#endregion
+
+//#region Patch Routes
 router.patch("/update/:playlistId", verifyJWT, updatePlaylist);
+//#endregion
+
+//#region Delete Routes
+router.delete("/delete/:playlistId", verifyJWT, deletePlaylist);
 router.delete("/remove-video/:playlistId", verifyJWT, removeVideoFromPlaylist);
+//#endregion
 
 export default router;
