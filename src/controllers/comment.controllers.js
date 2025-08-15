5954fdd2ab4ec9fda29d6d31fb7e9ad908144f3e
Oliver <softwaredevdad@gmail.com>
2025-08-15 11:13:47 +0200

    refactor logic to use aggregate as it is more efficient and we can achieve the same result but better and easier to read!

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -6,6 +6,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { Video } from "../models/video.models.js";
 import { Like } from "../models/like.models.js";
 
+//#region Get Video Comments
 const getVideoComments = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   const { page, limit } = req.query;
@@ -17,39 +18,54 @@ const getVideoComments = asyncHandler(async (req, res) => {
   const pageNum = parseInt(page) || 1;
   const limitNum = parseInt(limit) || 10;
 
-  const comments = await Comment.find({ video: videoId })
-    .populate("owner", "-password -email")
-    .skip((pageNum - 1) * limitNum)
-    .limit(limitNum)
-    .lean();
+  const userId = req.user?._id
+    ? new mongoose.Types.ObjectId(`${req.user._id}`)
+    : null;
 
-  if (!comments) {
-    throw new ApiError(404, "Comments not found");
-  }
+  const comments = await Comment.aggregate([
+    {
+      $match: { video: new mongoose.Types.ObjectId(`${videoId}`) },
+    },
+    { $sort: { createdAt: -1 } }, // newest first
+    { $skip: (pageNum - 1) * limitNum }, // paginate early
+    { $limit: limitNum },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [{ $project: { password: 0, email: 0 } }],
+      },
+    },
+    { $unwind: "$owner" },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "comment",
+        as: "likesList",
+      },
+    },
+    {
+      $addFields: {
+        likes: { $size: "$likesList" },
+        isLiked: userId ? { $in: [userId, "$likesList.likedBy"] } : false,
+        isOwner: userId ? { $eq: ["$owner._id", userId] } : false,
+      },
+    },
+    {
+      $project: {
+        likesList: 0, // remove raw likes array
+      },
+    },
+  ]);
 
-  const userId = req.user?._id?.toString();
-
-  // Add likes count and isLiked
-  const commentsWithLikes = await Promise.all(
-    comments.map(async (comment) => {
-      const likesCount = await Like.countDocuments({ comment: comment._id });
-      const isLiked = userId
-        ? await Like.exists({ comment: comment._id, likedBy: userId })
-        : false;
-
-      return {
-        ...comment,
-        isOwner: comment.owner._id.toString() === userId,
-        likes: likesCount,
-        isLiked: Boolean(isLiked),
-      };
-    }),
-  );
-
-  res
-    .status(200)
-    .json(new ApiResponse(200, commentsWithLikes, "Comments fetched"));
+  res.status(200).json(new ApiResponse(200, comments, "Comments fetched"));
 });
+//#endregion
+
+//#region Add Comment
 const addComment = asyncHandler(async (req, res) => {
   // TODO: add a comment to a video
   const { videoId } = req.params;
@@ -80,11 +96,13 @@ const addComment = asyncHandler(async (req, res) => {
     throw new ApiError(500, "Error adding comment", error);
   }
 });
+//#endregion
 
 // const updateComment = asyncHandler(async (req, res) => {
 //   // TODO: update a comment
 // });
 
+//#region Delete Comment
 const deleteComment = asyncHandler(async (req, res) => {
   // TODO: delete a comment
   const { id } = req.params;
@@ -106,5 +124,6 @@ const deleteComment = asyncHandler(async (req, res) => {
     throw new ApiError(500, "Error deleting comment", error);
   }
 });
+//#endregion
 
 export { getVideoComments, addComment, deleteComment };
