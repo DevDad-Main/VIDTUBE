5c03d40dbd1c95bc87822eca5332142e939747bb
Oliver <softwaredevdad@gmail.com>
2025-08-17 19:43:50 +0200

    finish unsubscribe logic

--- src/controllers/subscription.controllers.js
+++ src/controllers/subscription.controllers.js
@@ -5,7 +5,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-//#region Toggle Subscription
+//#region Subscribe
 const subscribe = asyncHandler(async (req, res) => {
   const { channelId } = req.body;
 
@@ -43,6 +43,44 @@ const subscribe = asyncHandler(async (req, res) => {
 });
 //#endregion
 
+//#region Unsubscribe
+const unSubscribe = asyncHandler(async (req, res) => {
+  const { channelId } = req.body;
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid Channel Id");
+  }
+
+  try {
+    const isSubscribedToChannel = await Subscription.find({
+      channel: channelId,
+      subscriber: req.user?._id,
+    });
+
+    if (!isSubscribedToChannel) {
+      throw new ApiError(404, "You are not subscribed to this channel");
+    }
+
+    console.log(isSubscribedToChannel);
+
+    const subscription = await Subscription.findOneAndDelete({
+      channel: channelId,
+      subscriber: req.user?._id,
+    });
+
+    if (!subscription) {
+      throw new ApiError(500, "Something went wrong on our end!");
+    }
+
+    return res
+      .status(201)
+      .json(new ApiResponse(201, {}, "Unsubscribed Succesfully"));
+  } catch (error) {
+    throw new ApiError(500, "Error subscribing", error);
+  }
+});
+//#endregion
+
 //#region Get User Channel Subscribers
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
@@ -55,4 +93,9 @@ const getSubscribedChannels = asyncHandler(async (req, res) => {
 });
 //#endregion
 
-export { subscribe, getUserChannelSubscribers, getSubscribedChannels };
+export {
+  subscribe,
+  unSubscribe,
+  getUserChannelSubscribers,
+  getSubscribedChannels,
+};
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -3,13 +3,18 @@ import {
   getSubscribedChannels,
   getUserChannelSubscribers,
   subscribe,
+  unSubscribe,
 } from "../controllers/subscription.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/c").get(getSubscribedChannels).post(subscribe);
+router
+  .route("/c")
+  .get(getSubscribedChannels)
+  .post(subscribe)
+  .delete(unSubscribe);
 
 router.get("/u/:subscriberId", getUserChannelSubscribers);
 
