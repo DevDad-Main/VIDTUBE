09f91a3ef22bb73e404f77573e8e82f411056b3a
Oliver <softwaredevdad@gmail.com>
2025-08-18 20:06:42 +0200

    change .env to production to allow secure to be true and add sameSite non as vercel works on pc but not mobile

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -342,6 +342,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     const options = {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
     };
 
     const { accessToken, refreshToken: newRefreshToken } =
