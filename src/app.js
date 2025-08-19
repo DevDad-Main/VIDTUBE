91bcd13ab349b62b8e2f3f9cdbce1fc1b61c7845
Oliver <softwaredevdad@gmail.com>
2025-08-19 11:16:22 +0200

    change cors route as now we are only using one

--- src/app.js
+++ src/app.js
@@ -22,20 +22,12 @@ const allowedOrigins = process.env.CORS_ORIGIN.split(","); // split comma-separa
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin like mobile apps or curl
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: process.env.CORS_ORIGIN,
     methods: ["PATCH", "POST", "PUT", "GET", "DELETE", "OPTIONS"],
     allowedHeaders: [
       "Content-Type",
       "Authorization",
-      // "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Headers",
     ],
     credentials: true,
   }),
