056a8ff21d72452fb86b81b61f4fcb9b05c8492b
Oliver <softwaredevdad@gmail.com>
2025-08-19 14:40:24 +0200

    add fs import(oopsy)

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -1,7 +1,7 @@
 import multer from "multer";
 import { fileURLToPath } from "url";
 import path from "path";
-
+import fs from "fs";
 //#region Constants
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
