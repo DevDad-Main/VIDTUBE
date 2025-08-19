dd4413c90b0f28646cd838e93e9ddaeb5c9db171
Oliver <softwaredevdad@gmail.com>
2025-08-19 13:49:41 +0200

    add image file filter by type

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -1,16 +1,30 @@
 import multer from "multer";
 
+//#region Multer Iamge Type Filter
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+//#endregion
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './public/temp')
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.originalname + '-' + uniqueSuffix)
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname + "-" + uniqueSuffix);
+  },
+  fileFilter: fileFilter,
+});
 
 export const upload = multer({
-    storage
-})
+  storage,
+});
