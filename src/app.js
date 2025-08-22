41214185426dffb3460ce8378f47262262c72a5e
Oliver <softwaredevdad@gmail.com>
2025-08-22 14:32:28 +0200

    fix path error and allow __dirname to dynamically work depending on what we use

--- src/app.js
+++ src/app.js
@@ -15,6 +15,10 @@ import helmet from "helmet";
 import morgan from "morgan";
 import compression from "compression";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
